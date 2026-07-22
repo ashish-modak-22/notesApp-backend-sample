6504c3e311d5e581ef253204cce550c0c7c26a89
Ashish Modak <toashish2006@gmail.com>
2026-07-23 03:55:47 +0530

    correct the wrong password for postgreSQL database

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,7 @@ from app.database import Base
 from app.models import *
 
 
-load_dotenv()
+load_dotenv(override=True)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
