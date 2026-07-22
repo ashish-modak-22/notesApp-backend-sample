42793674398b69543d6aedf5f6738da4dde59c1a
Ashish Modak <toashish2006@gmail.com>
2026-07-22 06:18:10 +0530

    connected the postgreSQL database with FastAPI

--- app/database.py
+++ app/database.py
@@ -1,5 +1,31 @@
 import os
-
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+
+
+# Load the environment variables
+load_dotenv()
+
+
+
+# Read the database URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+
+# Create the PostgreSQL engine
+engine = create_engine(
+    DATABASE_URL
+)
+
+
+
+# Create database session
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
