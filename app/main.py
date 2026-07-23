381258737cb757b235631dc5175f10d9cffe6acd
Ashish Modak <toashish2006@gmail.com>
2026-07-23 18:26:43 +0530

    taking user credentials and storing them inside database with password being hashed

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.database import get_db
 from app.schemas.user import UserRegister, UserResponse
 from app.models.user import User
 from fastapi import HTTPException
+from app.core.security import hash_password
 
 
 
@@ -35,3 +36,17 @@ async def register(
             status_code=400,
             detail="Email already registered"
         )
+    
+    hashed_password = hash_password(user.password)
+
+    new_user = User(
+        name = user.name,
+        email = user.email,
+        password_hash = hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
