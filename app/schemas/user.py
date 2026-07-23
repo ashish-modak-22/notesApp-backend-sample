ac39cf3de98660bf196e1d3424b0bc5f7edf1bbd
Ashish Modak <toashish2006@gmail.com>
2026-07-23 06:31:00 +0530

    added user registration and user response schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+
+
+
+
+class UserRegister(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+    class config:
+        from_attributes = True
