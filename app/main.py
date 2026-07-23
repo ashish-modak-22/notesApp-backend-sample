21a12edc9e0ccb42b40b372d34abd8341d80eb0e
Ashish Modak <toashish2006@gmail.com>
2026-07-23 20:04:10 +0530

    moved the response API from main.py to auth.py

--- app/main.py
+++ app/main.py
@@ -6,10 +6,14 @@ from app.schemas.user import UserRegister, UserResponse
 from app.models.user import User
 from fastapi import HTTPException
 from app.core.security import hash_password
+from app.routers import auth
 
 
 
 app  = FastAPI()
+
+
+app.include_router(auth.router)
 
 
 # Root endpoint that is called when the base URL ("/") is accessed
@@ -22,31 +26,3 @@ def home():
     }
 
 
-@app.post("/register", response_model=UserResponse)
-async def register(
-    user: UserRegister,
-    db: Session = Depends(get_db)
-):
-    existing_user = db.query(User).filter(
-        User.email == user.email
-    ).first()
-
-    if existing_user:
-        raise HTTPException(
-            status_code=400,
-            detail="Email already registered"
-        )
-    
-    hashed_password = hash_password(user.password)
-
-    new_user = User(
-        name = user.name,
-        email = user.email,
-        password_hash = hashed_password
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
