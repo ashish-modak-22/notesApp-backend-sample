b3568b90cd0ca6068af0ed40286e4849daacf182
Ashish Modak <toashish2006@gmail.com>
2026-07-24 13:30:29 +0530

    configure the JWT inside the secret.py file

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,18 @@
 import bcrypt
 from jose import jwt
 from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv(override=True)
+
+
+# Load the JWT configuration from the environment variables
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 
